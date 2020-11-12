b372afe38e03cded2260f2cbaa62811f456666a7
Ballo10 <61250147+Ballo10@users.noreply.github.com>
2020-11-12 17:01:20 +0100

    init

---
+++ server/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IO_lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TCPAsynchClasses.serverEchoA server = new TCPAsynchClasses.serverEchoA(IPAddress.Parse("127.0.0.1"), 2048);
+            server.Start();
+        }
+    }
+}
