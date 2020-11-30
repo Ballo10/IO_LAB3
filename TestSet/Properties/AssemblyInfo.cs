3ace3b4cad39e713072be0a12672fa66998bdf51
kaczmareka407 <kaczmareka407@gmail.com>
2020-11-30 19:57:11 +0100

    new test

---
+++ TestSet/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestSet")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestSet")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("130d6c17-4715-4c40-b7ef-96b500312155")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestSet/Testy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace TestSet
+{
+    [TestClass]
+    public class Testy
+    {
+        [TestMethod]
+        public void TestWrongPortNumber()
+        {
+            try
+            {
+                ServerLib.TextServerAsync server = new ServerLib.TextServerAsync(IPAddress.Parse("127.0.0.1"), 4000);
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+       /* [TestMethod]
+        public void TestMethod2()
+        {
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+        }*/
+    }
+}
