b912d45636e963486cbcb5189d5fb1b0eeb74220
Grzegorz Konowalczyk <gkonowalczyk@suncode.pl>
2020-11-30 22:09:25 +0100

    Added client app

---
+++ Client/Client.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class Client
+    {
+
+        private string ipAddress;
+        private UInt16 port;
+        private TcpClient client;
+
+        private NetworkStream netStream;
+
+        public Client(string ipAddress, UInt16 port, TcpClient client)
+        {
+            this.ipAddress = ipAddress;
+            this.port = port;
+            this.client = client;
+
+            netStream = client.GetStream();
+        }
+
+        public void Start()
+        {
+            Task task = Task.Run(() => {
+                this.StartReadThread();
+            });
+
+            for (; ; )
+            {
+                string message = Console.ReadLine();
+
+                if (!message.Equals("quit"))
+                {
+                    this.SendMessage(message);
+                }
+                else break;
+            }
+
+            netStream.Close();
+            client.Close();
+        }
+
+        private void StartReadThread()
+        {
+            int BUFFER_SIZE = 1024;
+            byte[] buffer = new byte[BUFFER_SIZE];
+
+            string stringBuffer = "";
+
+            for (; ; )
+            {
+                int readBytes = netStream.Read(buffer, 0, BUFFER_SIZE);
+                stringBuffer += Encoding.UTF8.GetString(buffer, 0, readBytes);
+
+                int newLineIndex;
+                while ((newLineIndex = stringBuffer.IndexOf("\r\n")) != -1)
+                {
+                    string message = stringBuffer.Substring(0, newLineIndex);
+                    stringBuffer = stringBuffer.Substring(newLineIndex + 2);
+
+                    Console.WriteLine(message);
+                }
+            }
+        }
+
+        public void SendMessage(string message)
+        {
+            byte[] finalMessage = Encoding.UTF8.GetBytes(message + "\r\n");
+            this.netStream.Write(finalMessage, 0, finalMessage.Length);
+        }
+
+    }
+}
---
+++ Client/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string ipAddress = null;
+            UInt16 port = 0;
+            TcpClient tcpClient = null;
+
+            bool valid = false;
+            while (!valid)
+            {
+                try
+                {
+                    Console.WriteLine("Podaj adres ip");
+                    ipAddress = Console.ReadLine();
+
+                    Console.WriteLine("Podaj port");
+                    port = Convert.ToUInt16(Console.ReadLine());
+
+                    tcpClient = new TcpClient(ipAddress, port);
+                    valid = true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Błąd: " + e.Message);
+                    Console.WriteLine("Spróbuj ponownie.");
+                }
+            }
+
+            Console.WriteLine("Połączono pomyślnie.");
+
+            Client client = new Client(ipAddress, port, tcpClient);
+            client.Start();
+        }
+
+    }
+}
